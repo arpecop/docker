cc495a6c586ed87fd233dde7a09388d5d532ac8d
Radoslav <rudix.lab@gmail.com>
2020-12-29 20:02:06 +0200

    c

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ WORKDIR /home/node/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 8080
+EXPOSE 3000
 CMD ["node", "server.js" ]
--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(8080, () => {
+app.listen(3000, () => {
   console.log(`Example app listening at http://localhost`);
 });
